fd8574de912e6c6f9fa9031b38dcd2b234affefe
marufalaslam <maruf14@cse.pstu.ac.bd>
2024-02-06 16:40:26 +0600

    slider added

---
+++ js/app.js
@@ -0,0 +1,30 @@
+const navigatorButtons = document.querySelectorAll(".navigator button");
+
+navigatorButtons.forEach((button) => {
+  button.addEventListener("click", () => {
+    // remove active class from all buttons
+    navigatorButtons.forEach((button) => {
+      button.classList.remove("active");
+    });
+
+    // add active class to the clicked button
+    button.classList.add("active");
+
+    // get button's text
+    const buttonText = button.textContent;
+
+    // get left and right divs with id #leftContent and #rightContent
+    const leftContent = document.getElementById("left"+buttonText);
+    const rightContent = document.getElementById("right"+buttonText);
+
+    // remove active class from all divs
+    const allContents = document.querySelectorAll(".content");
+    allContents.forEach((content) => {
+      content.classList.remove("active");
+    });
+
+    // make active
+    leftContent.classList.add("active");
+    rightContent.classList.add("active");
+  });
+});
